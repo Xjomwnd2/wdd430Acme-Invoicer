ce6780ed405b3f0972bd180caec3b00c70f8e218
Joel Ndiba Mwaura <mwa21001@byui.edu>
2025-03-11 20:41:24 +0300

    COMMIT

---
+++ components/Customers.js
@@ -0,0 +1,8 @@
+export function Customers() {
+    return (
+      <div>
+        <h1 className="text-2xl font-bold">Customers</h1>
+        <p>List of customers here...</p>
+      </div>
+    );
+  }
