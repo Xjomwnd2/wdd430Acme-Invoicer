a20e8c18fbc5990b9e88848d9240adeda3b2efaa
Joel Ndiba Mwaura <mwa21001@byui.edu>
2025-03-11 22:13:39 +0300

    commit

---
+++ pages/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="p-6 max-w-2xl mx-auto text-center">
+      <h1 className="text-3xl font-bold text-blue-600">About Us</h1>
+      <p className="mt-4 text-gray-700">
+        Welcome to our company! We are committed to providing the best services 
+        to our customers. Our mission is to deliver high-quality products and 
+        exceptional customer support.
+      </p>
+    </div>
+  );
+};
+
+export default About;
