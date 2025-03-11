2c30f58a041fbc603e2ddf0aad42e8a9f76226d3
Joel Ndiba Mwaura <mwa21001@byui.edu>
2025-03-11 20:37:38 +0300

    COMMIT

---
+++ components/Dashboard.js
@@ -0,0 +1,8 @@
+export function Dashboard() {
+    return (
+      <div>
+        <h1 className="text-2xl font-bold">Dashboard</h1>
+        <p>Recent Revenue: $2,689.26</p>
+      </div>
+    );
+  }
