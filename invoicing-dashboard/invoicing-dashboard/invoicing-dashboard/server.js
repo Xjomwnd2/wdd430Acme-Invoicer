a9a007f76f75803b4c9c417abd924ba13afcc318
Joel Ndiba Mwaura <mwa21001@byui.edu>
2025-03-11 16:05:50 +0300

    commit

--- invoicing-dashboard/invoicing-dashboard/invoicing-dashboard/server.js
+++ invoicing-dashboard/invoicing-dashboard/invoicing-dashboard/server.js
@@ -1,11 +1,16 @@
+require('dotenv').config(); // Ensure dotenv is installed
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
-const PORT = 3000;
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello, Invoicing Dashboard!');
+    res.send('Server is running!');
 });
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
